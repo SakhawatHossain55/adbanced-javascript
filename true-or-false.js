16ad0dd1094971f229fce52a41aff6d00419d939
SakhawatHossain55 <76767141+SakhawatHossain55@users.noreply.github.com>
2021-02-02 08:09:20 +0600

    true or false

---
+++ true-or-false.js
@@ -0,0 +1,17 @@
+// falsy value
+// false
+// 0
+// ''
+// null
+// NaN
+// truthy value
+// '0'
+// ' '
+let name = NaN;
+console.log(name);
+if(name){
+    console.log('Condition is true')
+}
+else{
+    console.log('condition is false,')
+}
